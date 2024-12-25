160cb658446dde630d15682f6ab7f5f5a765ce20
Amin <wlaminn84@gmail.com>
2024-12-25 14:17:48 +0330

    FixResponsive

--- src/style/script.js
+++ src/style/script.js
@@ -1,7 +1,1 @@
-function toggleMenu() {
-    const hamburger = document.querySelector('.hamburger');
-    const menu = document.getElementById('menu');
 
-    hamburger.classList.toggle('active');
-    menu.classList.toggle('active');
-}
